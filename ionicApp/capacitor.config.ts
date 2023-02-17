aee06b257e6080aed75f3b6d314c5342bc773e51
azori3 <ali.methnani@esprit.tn>
2023-02-17 23:23:18 +0100

    create ionic project

--- ionicApp/capacitor.config.ts
+++ ionicApp/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ionicApp',
-  webDir: 'build',
+  webDir: 'www',
   bundledWebRuntime: false
 };
 
